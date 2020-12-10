f8382faf009d882458d0cbd4951a0b68e2b10e75
Yasemin Gerboğa <yasemingerboga@gmail.com>
2020-12-10 20:04:56 +0300

    split strings using multiple seperate characters.

---
+++ Split_String/Split.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class Split
+{
+private:
+	string const delims{ "-+" };
+	size_t beg, pos = 0;
+public:
+	Split();
+	~Split();
+	string* splitOperations(string,string[]);
+};
+
